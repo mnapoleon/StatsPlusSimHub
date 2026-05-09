09d098de6c0d050b2cc76c931697ef1754483004
Michael Napoleon <michael.napoleon@gmail.com>
2026-05-09 16:11:07 -0400

    Merge pull request #1 from mnapoleon/codex-affinity-settings-tabs
    Rework Affinity settings and debug logging

--- Affinity/AffinitySettings.cs
+++ Affinity/AffinitySettings.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Affinity
 {
     public class AffinitySettings
@@ -6,10 +8,16 @@ namespace Affinity
 
         public bool DisplayInMiles { get; set; }
 
+        public bool EnableDebugLogging { get; set; }
+
+        public Dictionary<string, bool> GameDebugLogging { get; set; } = new Dictionary<string, bool>();
+
         public void Reset()
         {
             EnablePlugin = true;
             DisplayInMiles = false;
+            EnableDebugLogging = false;
+            GameDebugLogging = new Dictionary<string, bool>();
         }
     }
 }
